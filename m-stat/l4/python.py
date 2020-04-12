1228ace9447418ff37620ea6ec2fb8513b8a480f
aleksey <alexlyachko@gmail.com>
2020-04-12 17:28:27 +0300

    dfsadfs

--- m-stat/l4/python.py
+++ m-stat/l4/python.py
@@ -1,16 +1,21 @@
 import random
 import math
 
-def func(x):
-    T = 8760;
-    return 
-    ((((x[0] > T) & (x[1] > T)) | ((x[2] > T) & (x[3] > T))) & 
-    (x[4] > T) & (x[5] > T) & 
-    ( ((x[6] > T) & (x[7] > T)) | ((x[8] > T) & (x[9] > T)) | ((x[10] > T) & (x[11] > T)) )); 
+def func(T,x):
+    return (
+        x[0] > T and x[1] > T or x[2] > T and x[3] > T and 
+        x[4] > T and 
+        x[5] > T and 
+        x[6] > T and x[7] > T or x[8] > T and x[9] > T or x[10] > T and x[11] > T 
+        ); 
+    # return (
+    #     (x[0] > T and x[1] > T) or (x[2] > T and x[3] > T) and 
+    #     (x[4] > T) and 
+    #     (x[5] > T) and 
+    #     ( (x[6] > T and x[7] > T) or (x[8] > T and x[9] > T) or (x[10] > T and x[11] > T) ) 
+    #     ); 
 
-
-def work(L):
-    N = 33012
+def work(N, T, L):
     m = 3
     lambdas = [40 * 10 ** -6, 10 * 10 ** -6, 80 * 10 ** -6]
     n = [4,2,6]
@@ -22,7 +27,7 @@ def work(L):
         for i in range(0,m):
             t = []
             for j in range(0,n[i]):
-                a =random.random()
+                a = random.random()
                 t.append(-math.log(a) / lambdas[i])
 
             for j in range(0, L[i]):
@@ -32,17 +37,24 @@ def work(L):
             for j in range(0,n[i]):
                 x.append(t[j])
         
-        if not func(x):
+        if not func(T,x):
             d = d + 1
-            # print(d)
-
-    return 1 - (d / N);
+    
+    return 1 - d / N
 
 P0 = 0.999
+TT = 8760
 L = [0,0,0]
 
-wtb = 5
-wtf = 10
+alpha = 99.9
+eps = 0.001
+tau = 3.090
+
+NN = math.trunc(tau**2 * ( ((1 - P0) * P0) / eps ** 2 ))
+print("N = ", NN)
+
+wtb = 0
+wtf = 5
 
 for i in range (wtb,wtf):
     L[0] = i
@@ -53,12 +65,9 @@ for i in range (wtb,wtf):
         for k in range (wtb,wtf):
             
             L[2] = k
-            P = work(L)
+            P = work(NN,TT,L)
 
             # if (P > P0):
-            #     print("LOOK ABOVE")
             #     print("P = ", P, "L = ", " ", sum(L))
             
-            print("P = ", P, "L = ", " ", sum(L))
-
-
+            print("P = ", P, "L = ", L, " ", sum(L))
